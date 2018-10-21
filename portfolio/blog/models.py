79d39d3b6ad45df26ac8a0f81af73d8858c44183
mitul patel <mitulpatel3939@gmail.com>
2018-10-21 18:29:26 +0530

    backend commit

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+class blog(models.Model):
+    title=models.CharField(max_length=100)
+    pub_date=models.DateTimeField(("date"),auto_now=False)
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
