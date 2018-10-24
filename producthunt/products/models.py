c784f326cc388696f8e87f103143d744aedf85ae
mitul patel <mitulpatel3939@gmail.com>
2018-10-24 12:44:41 +0530

    complete portfolio

--- producthunt/products/models.py
+++ producthunt/products/models.py
@@ -1,11 +1,19 @@
 from django.db import models
-from datetime import date
+from django.contrib.auth.models import User
 # Create your models here.
-class blog(models.Model):
+
+
+
+# Create your models here.
+class Product(models.Model):
     title=models.CharField(max_length=100)
-    pub_date=models.DateTimeField(("date"),auto_now=False)
+    pub_date=models.DateTimeField()
     body=models.TextField()
+    url= models.TextField()
     image=models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total=models.IntegerField(default=1)
+    hunter = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
     def summary(self):
